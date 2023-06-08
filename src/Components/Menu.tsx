3dfb7483df3d925b6eb11a787e9f07e779f38d7b
Illia <ilyusha.drozd.2016@gmail.com>
2023-06-08 21:08:40 +0300

    test nav add for ci/cd check

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -8,6 +8,7 @@ const Menu = () => {
       <NavLink to="dogs">Dogs</NavLink>
       <NavLink to="about">About</NavLink>
       <NavLink to="support">Support</NavLink>
+      <a> not working test nav</a>
     </nav>
   )
 }

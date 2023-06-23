78b440e848bbffe7c5020bb39d7b56cc1c390e49
Natalia <nataliamaliovana.nm@gmail.com>
2023-06-23 21:29:42 +0700

    Created footer component and ShelterEvend section in home page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Slider from './Components/Slider'
 import { GridContainer, StyledHome } from './Home.styles'
+import ShelterEvents from './Components/ShelterEvents'
 
 function Home() {
   return (
     <StyledHome data-testid="home-page">
       <GridContainer>
         <Slider />
+        <ShelterEvents />
       </GridContainer>
     </StyledHome>
   )

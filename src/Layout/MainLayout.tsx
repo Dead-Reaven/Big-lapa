3d504303dd9b90edb9b47abf5fe4933d8ff9895d
Illia <ilyusha.drozd.2016@gmail.com>
2023-06-23 18:05:02 +0300

    deleted container in layout

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -1,18 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../Components/Header/Header'
 import ResetStyles from '../styles/Global.styles'
-import Container from '../styles/Container.style'
 import Footer from '../Components/Footer/Footer'
 
 function MainLayout() {
   return (
     <>
-      <Container>
-        <ResetStyles />
-        <Header />
-        <Outlet />
-        <Footer />
-      </Container>
+      <ResetStyles />
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   )
 }

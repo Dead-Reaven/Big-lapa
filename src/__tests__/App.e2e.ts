65f40cb0383440b877c9faae62036692a0134a23
Illia <ilyusha.drozd.2016@gmail.com>
2023-06-23 19:39:59 +0300

    remove all tests to __tests__ folder

--- src/__tests__/App.e2e.ts
+++ src/__tests__/App.e2e.ts
@@ -10,6 +10,7 @@ describe('User test App.e2e.ts', () => {
     browser = await puppeteer.launch({ headless: 'new' })
     page = await browser.newPage()
   })
+  afterAll(async () => await browser.close())
 
   it('contains This is home page', async () => {
     await page.goto('http://localhost:3000')
@@ -18,5 +19,46 @@ describe('User test App.e2e.ts', () => {
     expect(text).toContain('Подаруй другу шанс')
   })
 
-  afterAll(() => browser.close())
+  describe('Test header navigate to other pages', () => {
+    it('should navigate to the home page', async () => {
+      const link = await page.waitForSelector('[data-testid="home-link"]')
+      link?.click()
+      await page.waitForNavigation()
+      const currentURL = page.url()
+      expect(currentURL).toBe('http://localhost:3000/Big-lapa/')
+    })
+
+    it('should navigate to the dogs page', async () => {
+      const link = await page.waitForSelector('[data-testid="dogs-link"]')
+      link?.click()
+      await page.waitForNavigation()
+      const currentURL = page.url()
+      expect(currentURL).toBe('http://localhost:3000/Big-lapa/dogs')
+    })
+
+    it('should navigate to the about page', async () => {
+      const link = await page.waitForSelector('[data-testid="about-link"]')
+      link?.click()
+      await page.waitForNavigation()
+      const currentURL = page.url()
+      expect(currentURL).toBe('http://localhost:3000/Big-lapa/about')
+    })
+
+    it('should navigate to the contacts page', async () => {
+      const link = await page.waitForSelector('[data-testid="contacts-link"]')
+      link?.click()
+      await page.waitForNavigation()
+      const currentURL = page.url()
+      expect(currentURL).toBe('http://localhost:3000/Big-lapa/contacts')
+    })
+    it('should navigate to the 404 page', async () => {
+      await page.goto('http://localhost:3000/Big-lapa/mistakepage')
+      await page.waitForSelector('[data-testid="not-found-page"]')
+      const text = await page.$eval(
+        '[data-testid="not-found-page"]',
+        (e) => e.textContent,
+      )
+      expect(text).toContain('404 NotFound')
+    })
+  })
 })

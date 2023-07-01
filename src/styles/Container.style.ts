2e093214e991ae4d3d2602b5d75883777f217178
Illia <ilyusha.drozd.2016@gmail.com>
2023-07-01 15:23:12 +0300

    add scalable=no to render page in original size

--- src/styles/Container.style.ts
+++ src/styles/Container.style.ts
@@ -1,5 +1,5 @@
 import { styled } from 'styled-components'
-import device from '../styles/Sizes'
+// import device from '../styles/Sizes'
 
 interface Props {
   fontSize?: string
@@ -8,26 +8,38 @@ interface Props {
   color?: string
   padding?: string
 }
+const size = {
+  mobile: '393px',
+  tablet: '768px',
+  laptop: '1280px',
+  desktop: '1440px',
+}
+
+const device = {
+  mobile: `(min-width: ${size.mobile})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  desktop: `(max-width: ${size.desktop})`,
+}
 
 const Container = styled.div<Props>`
-  width: 100%;
   margin: 0 auto;
   padding: ${({ theme, padding }) => padding || theme.paddings.container};
   padding-top: ${({ paddingtop }) => paddingtop || '0'};
   padding-bottom: ${({ paddingbottom }) => paddingbottom || '0'};
   font-size: ${({ fontSize, theme }) => fontSize || theme.fontSizes.bodyText};
 
-  @media screen and ${device.mobile} {
-    width: 393px;
+  @media screen and (${device.mobile}) {
+    max-width: 393px;
   }
-  @media screen and ${device.tablet} {
-    width: 768px;
+  @media screen and (${device.tablet}) {
+    max-width: 768px;
   }
-  @media screen and ${device.laptop} {
-    width: 1280px;
+  @media screen and (${device.laptop}) {
+    max-width: 1280px;
   }
-  @media screen and ${device.desktop} {
-    width: 1440px;
+  @media screen and (${device.desktop}) {
+    max-width: 1440px;
   }
 `
 export default Container

c3d6aa8f1feadab8b0df53b1d305bebcb003fcec
Natalia <nataliamaliovana.nm@gmail.com>
2023-07-01 18:22:28 +0700

    Sizes files with media queries

--- src/styles/Sizes.ts
+++ src/styles/Sizes.ts
@@ -9,7 +9,7 @@ const device = {
   mobile: `(min-width: ${size.mobile})`,
   tablet: `(min-width: ${size.tablet})`,
   laptop: `(min-width: ${size.laptop})`,
-  desktop: `(min-width: ${size.desktop})`,
+  desktop: `(max-width: ${size.desktop})`,
 }
 
 export default device

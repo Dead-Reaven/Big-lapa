447ffc50ad4fa907cbe05893921a7d877570203e
jnkdi <anaidik11@gmail.com>
2023-07-03 17:40:48 +0300

    small fixes

--- src/styles/Container.style.ts
+++ src/styles/Container.style.ts
@@ -10,7 +10,7 @@ interface Props {
 }
 
 const Container = styled.div<Props>`
-  width: 100%;
+  width: 100vw;
   max-width: 1440px;
   margin: 0 auto;
   padding: ${({ theme, padding }) => padding || theme.paddings.container};

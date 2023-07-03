122945cbde1e13aae200d360020d25e876b8d9b2
Illia <ilyusha.drozd.2016@gmail.com>
2023-07-03 21:36:04 +0300

    add responsible-adaptive layout in slider

--- src/Pages/Home/Components/Slider/Slider.style.ts
+++ src/Pages/Home/Components/Slider/Slider.style.ts
@@ -8,44 +8,134 @@ const SliderStyled = styled.div`
   color: ${theme.colors.background};
   background-color: ${theme.colors.green};
 `
-const TextContainer = styled.div`
+const TextContainer = styled.pre`
+  width: 49%;
+  padding: 10% 1% 0 0;
   display: flex;
   flex-direction: column;
+  justify-items: center;
   flex: auto 1 0;
   gap: 10%;
-  margin-top: 10%;
+  word-wrap: break-word;
+  & svg {
+    display: inline-block;
+    z-index: 99;
+  }
 `
-const FlexContainer = styled(Container)`
+
+const H1 = styled.h1`
+  font-size: 3.8vw;
+  /* max-width: 54px; */
+  font-weight: 700;
+  line-height: 120%;
+  @media screen and (min-width: 1440px) {
+    font-size: 54px;
+  }
+  @media screen and (min-width: 300px) and (max-width: 767px) {
+    font-size: 6vw;
+  }
+`
+
+const SubText = styled.h3`
+  display: flex;
+  flex: auto 1 0;
+  font-size: 1.25vw;
+  line-height: 140%;
+  @media screen and (min-width: 1440px) {
+    font-size: 18px;
+  }
+  @media screen and (min-width: 1050px) and (max-width: 1200px) {
+    font-size: 1.4vw;
+  }
+  @media screen and (min-width: 850px) and (max-width: 1050px) {
+    font-size: 1.45vw;
+  }
+  @media screen and (min-width: 768px) and (max-width: 850px) {
+    font-size: 1.5vw;
+  }
+  @media screen and (min-width: 300px) and (max-width: 767px) {
+    display: block;
+    font-size: 2.3vw;
+  }
+`
+
+const YellowBlock = styled.div`
+  width: 49%;
+  height: 100%;
+  background: ${theme.colors.yellow} url(${mainPhoto}) bottom no-repeat;
+  background-size: contain;
+  border-radius: 3%;
+  z-index: 0;
+`
+const Flex = styled.div`
   height: 100%;
   display: flex;
   flex-shrink: 0;
   justify-content: space-between;
-  padding: 2.5% 5.83%;
+`
 
-  @media screen and (min-width: 1280px) and (max-width: 1440px) {
-    ${TextContainer} {
-      /*  */
+const StyledContainer = styled(Container)`
+  height: 100%;
+  padding: 2.5% 5.83%;
+  @media screen and (min-width: 1440px) {
+    padding: 40px 84px;
+  }
+
+  @media screen and (min-width: 1050px) and (max-width: 1200px) {
+    & {
+      padding: 5% 5.83%;
+    }
+  }
+  @media screen and (min-width: 850px) and (max-width: 1050px) {
+    & {
+      padding: 7% 5.83%;
+    }
+  }
+  @media screen and (min-width: 768px) and (max-width: 850px) {
+    & {
+      padding: 9% 5.83%;
+    }
+    ${H1} {
+      padding-top: 15%;
+    }
+  }
+  @media screen and (min-width: 300px) and (max-width: 767px) {
+    padding: calc(25px + 1vw) 5.83% calc(15px + 1vw);
+    ${Flex} {
+      flex-direction: column;
+      align-items: center;
+      justify-items: center;
+      position: relative;
+      ${TextContainer} {
+        text-align: center;
+        height: 100%;
+        width: 100%;
+        padding: 0;
+        position: relative;
+        gap: 5%;
+        ${H1} {
+          width: 100%;
+          font-size: calc(20px + 3vw);
+          padding: 0;
+        }
+        ${SubText} {
+          font-size: calc(8px + 1vw);
+        }
+        svg {
+          align-self: center;
+          margin-bottom: 1%;
+        }
+      }
+
+      ${YellowBlock} {
+        position: absolute;
+        bottom: 0px;
+        z-index: 0;
+        width: 100%;
+        height: 60%;
+      }
     }
   }
 `
 
-const H1 = styled.pre`
-  font-size: 3.8vw;
-  font-weight: 700;
-  line-height: 120%;
-`
-const SubText = styled.pre`
-  display: flex;
-  font-size: 1.25vw;
-  line-height: 140%;
-  flex: auto 1 0;
-`
-
-const YellowBlock = styled.div`
-  width: 48%;
-  background: ${theme.colors.yellow} url(${mainPhoto}) bottom no-repeat;
-  background-size: contain;
-  border-radius: 5%;
-`
-
-export { SliderStyled, YellowBlock, SubText, H1, TextContainer, FlexContainer }
+export { SliderStyled, YellowBlock, SubText, H1, TextContainer, StyledContainer, Flex }
--- src/Pages/Home/Components/Slider/Slider.tsx
+++ src/Pages/Home/Components/Slider/Slider.tsx
@@ -4,27 +4,30 @@ import {
   H1,
   SubText,
   TextContainer,
-  FlexContainer,
+  Flex,
+  StyledContainer,
 } from './Slider.style'
 import { ReactComponent as Bullet } from '../../../../assets/img/bullet_first.svg'
 
 function Slider() {
   return (
     <SliderStyled>
-      <FlexContainer>
-        <TextContainer>
-          <H1>
-            Подаруй другу шанс, <br />
-            врятуй тварину!
-          </H1>
-          <SubText>
-            Допоможи домашнім тваринам повернутся додому. <br />
-            Ми розділяємо щастя, об&apos;єднуючи тварин та їх господарів.
-          </SubText>
-          <Bullet />
-        </TextContainer>
-        <YellowBlock />
-      </FlexContainer>
+      <StyledContainer>
+        <Flex>
+          <TextContainer>
+            <H1>
+              Подаруй другу шанс, <br />
+              врятуй тварину!
+            </H1>
+            <SubText>
+              Допоможи домашнім тваринам повернутся додому. <br />
+              Ми розділяємо щастя, об&apos;єднуючи тварин та їх господарів.
+            </SubText>
+            <Bullet />
+          </TextContainer>
+          <YellowBlock />
+        </Flex>
+      </StyledContainer>
     </SliderStyled>
   )
 }

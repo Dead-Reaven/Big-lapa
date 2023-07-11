e0a7f96f01e3435db42abf51a040335ea404ece1
Illia <ilyusha.drozd.2016@gmail.com>
2023-07-11 17:19:54 +0300

    set 40px padding top in admin panel

--- src/Pages/Admin/Components/SideBar/SideBar.style.ts
+++ src/Pages/Admin/Components/SideBar/SideBar.style.ts
@@ -3,11 +3,10 @@ import { styled } from 'styled-components'
 const StyledSideBar = styled.div`
   display: flex;
   flex-direction: column;
-  width: 50%;
-  /* max-width: 500px; */
+  width: 350px;
 
   height: calc(100vh - 96px);
-  padding: 80px 17px 0 0;
+  padding: 40px 17px 0 0;
   overflow-y: auto;
   border-right: 1px solid #cdcdcd66;
 `

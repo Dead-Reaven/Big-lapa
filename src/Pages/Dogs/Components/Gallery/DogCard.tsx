801646277891fcb36bd4c14168308d2573b31de0
jnkdi <anaidik11@gmail.com>
2023-07-13 20:18:37 +0300

    fix: redirect to dog page

--- src/Pages/Dogs/Components/Gallery/DogCard.tsx
+++ src/Pages/Dogs/Components/Gallery/DogCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { ReactComponent as Paws } from './img/Paws.svg'
 import { StyledDogCard } from './Gallery.style'
 import TitleH3 from '../../../../Components/UI/TitleH3.styles'
@@ -9,16 +9,14 @@ interface Props {
 }
 
 function DogCard({ src }: Props) {
-  const navigate = useNavigate()
-  const navigateToDog = () => {
-    navigate('/dog')
-  }
   return (
-    <StyledDogCard onClick={navigateToDog}>
-      <img src={src} alt="dog" />
-      <TitleH3>Скай</TitleH3>
-      <p>Хлопчик, 2 роки</p>
-      <Paws />
+    <StyledDogCard >
+      <Link to="/dog">
+        <img src={src} alt="/dog" />
+        <TitleH3>Скай</TitleH3>
+        <p>Хлопчик, 2 роки</p>
+        <Paws />
+      </Link>
     </StyledDogCard>
   )
 }

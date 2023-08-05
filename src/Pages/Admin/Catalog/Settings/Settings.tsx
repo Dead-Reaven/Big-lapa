5bef6bd9a261f3bc53319193225626f2d182bb9f
Igor <shtefan.i333@gmail.com>
2023-08-05 14:23:39 +0300

    fixConflict

--- src/Pages/Admin/Catalog/Settings/Settings.tsx
+++ src/Pages/Admin/Catalog/Settings/Settings.tsx
@@ -1,39 +1,43 @@
 import { useState } from 'react'
-import usePatch from '../../../../API/usePatch'
+import usePatch from '../../../../API/fetchers/patchPatch'
 import { Form, FormButton, FormContainer, FormH2 } from '../../Components/UI/Form.style'
 import Input from '../../Components/UI/Input'
 
 function Settings() {
-  const [oldPassword, setOldPassword] = useState('')
-  const [newPassword, setNewPassword] = useState('')
+  const [passwordOld, setPasswordOld] = useState('')
+  const [passwordNew, setPasswordNew] = useState('')
+  const [worning, setWorning] = useState('')
 
   const handlerPatch = () => {
-    const patchReq = {
-      passwordOld: oldPassword,
-      passwordNew: newPassword,
+    const updateData = {
+      oldPassword: passwordOld,
+      newPassword: passwordNew,
+      setWorning,
     }
-    usePatch(patchReq)
+    usePatch(updateData)
   }
   return (
     <Form onSubmit={() => handlerPatch()}>
       <FormH2>Налаштування</FormH2>
       <p style={{ fontWeight: '700', marginBottom: '20px' }}>Змінити пароль</p>
       <FormContainer>
+        {worning && <h1 style={{ color: 'red' }}>Error: {worning}</h1>}
+
         <Input
-          state={oldPassword}
-          onChange={(newValue) => setOldPassword(newValue)}
+          state={passwordOld}
+          onChange={(newValue) => setPasswordOld(newValue)}
           label="Введіть старий пароль"
           placeholder="*********"
         />
         <Input
-          state={newPassword}
-          onChange={(newValue) => setNewPassword(newValue)}
+          state={passwordNew}
+          onChange={(newValue) => setPasswordNew(newValue)}
           label="Введіть новий пароль"
           placeholder="*********"
         />
         <Input
-          state={newPassword}
-          onChange={(newValue) => setNewPassword(newValue)}
+          state={passwordNew}
+          onChange={(newValue) => setPasswordNew(newValue)}
           label="Підтвердіть новий пароль"
           placeholder="*********"
         />

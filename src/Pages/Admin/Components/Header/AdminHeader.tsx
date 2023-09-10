d5360431d5b11c1a0e13c88cc8cea228e9a2a2e5
Igor <shtefan.i333@gmail.com>
2023-09-10 22:24:33 +0300

    new Logo admin panel

--- src/Pages/Admin/Components/Header/AdminHeader.tsx
+++ src/Pages/Admin/Components/Header/AdminHeader.tsx
@@ -1,12 +1,13 @@
 import { NavLink } from 'react-router-dom'
-import { Icon, NamePosition, StyledHeader } from './AdminHeader.style'
+import { NamePosition, StyledHeader } from './AdminHeader.style'
+import { ReactComponent as Logo } from './logo_header.svg'
 
 function AdminHeader() {
   const userName = localStorage.getItem('user_name')
   return (
     <StyledHeader>
       <NavLink to="." end>
-        <Icon />
+        <Logo />
       </NavLink>
       <NamePosition>{userName}</NamePosition>
     </StyledHeader>
